78c407815fdcb4f743af9f057961ea9612119548
likith <likith@madstreetden.com>
2022-12-18 23:40:37 +0530

    updates

--- aws/dynamodb_init.py
+++ aws/dynamodb_init.py
@@ -97,9 +97,7 @@ class AwsDynamodbOperations:
     def query_all():
         dynamodb = boto3.resource(
             'dynamodb',
-            region_name='us-east-1',
-            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+            region_name='us-east-1'
         )
         table = dynamodb.Table('tasks')
         response = table.scan()
