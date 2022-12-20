18a8d8ce35345a576492e013f70a675b5ee51c17
likith <likith@madstreetden.com>
2022-12-20 12:24:41 +0530

    updates

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -10,37 +10,37 @@ PROJECT_BASE_PATH='/usr/local/apps/taskApi'
 locale-gen en_GB.UTF-8
 
 # Install Python, SQLite and pip
-echo "Installing dependencies..."
-apt-get update
-apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
+#echo "Installing dependencies..."
+#apt-get update
+#apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
 # Create project directory
-mkdir -p $PROJECT_BASE_PATH
-git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
+#mkdir -p $PROJECT_BASE_PATH
+#git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
 # Create virtual environment
-mkdir -p $PROJECT_BASE_PATH/env
-python3 -m venv $PROJECT_BASE_PATH/env
+#mkdir -p $PROJECT_BASE_PATH/env
+#python3 -m venv $PROJECT_BASE_PATH/env
 
 #install python packages
 $PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
-$PROJECT_BASE_PATH/env/bin/pip install uwsgi==2.0.20
+#$PROJECT_BASE_PATH/env/bin/pip install uwsgi==2.0.20
 
 # run migrations
-cd $PROJECT_BASE_PATH
-$PROJECT_BASE_PATH/env/bin/python manage.py migrate
-$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+#cd $PROJECT_BASE_PATH
+#$PROJECT_BASE_PATH/env/bin/python manage.py migrate
+#$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 
 # Setup Supervisor to run our uwsgi process.
-cp $PROJECT_BASE_PATH/deploy/supervisor_taskApi.conf /etc/supervisor/conf.d/taskApi.conf
-supervisorctl reread
-supervisorctl update
-supervisorctl restart taskApi
+#cp $PROJECT_BASE_PATH/deploy/supervisor_taskApi.conf /etc/supervisor/conf.d/taskApi.conf
+#supervisorctl reread
+#supervisorctl update
+#supervisorctl restart taskApi
 
 # Setup nginx to make our application accessible.
-cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/taskApi.conf
-rm /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/taskApi.conf /etc/nginx/sites-enabled/taskApi.conf
-systemctl restart nginx.service
+#cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/taskApi.conf
+#rm /etc/nginx/sites-enabled/default
+#ln -s /etc/nginx/sites-available/taskApi.conf /etc/nginx/sites-enabled/taskApi.conf
+#systemctl restart nginx.service
 
 echo "DONE! :)"
